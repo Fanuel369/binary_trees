e496cd256efb93660851b31994fb994d6780dae0
jzamora5 <jzamora_5@yahoo.com>
2020-03-12 04:42:11 +0000

    Revert Test Changes

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * avl_rotation - applies rotation for insert
  * @tree: double pointer to the root node of the AVL tree for
@@ -34,16 +35,26 @@ void avl_rotation(avl_t **tree, int value)
 }
 
 /**
- * avl_insert2 - inserts a value in an AVL Tree
+ * avl_insert - inserts a value in an AVL Tree
  * @tree: double pointer to the root node of the AVL tree for
  * inserting the value
  * @value: value to store in the node to be inserted
  *
  * Return: pointer to the created node, or NULL on failure
  */
-avl_t *avl_insert2(avl_t **tree, int value)
+avl_t *avl_insert(avl_t **tree, int value)
 {
+
 	avl_t *node = NULL;
+
+	if (!tree)
+		return (NULL);
+
+	if (!(*tree))
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
 
 	if (value < (*tree)->n)
 	{
@@ -68,28 +79,6 @@ avl_t *avl_insert2(avl_t **tree, int value)
 
 	if (node)
 		avl_rotation(tree, value);
+
 	return (node);
-
-}
-/**
- * avl_insert - inserts a value in an AVL Tree
- * @tree: double pointer to the root node of the AVL tree for
- * inserting the value
- * @value: value to store in the node to be inserted
- *
- * Return: pointer to the created node, or NULL on failure
- */
-avl_t *avl_insert(avl_t **tree, int value)
-{
-
-	if (!tree)
-		return (NULL);
-
-	if (!(*tree))
-	{
-		*tree = binary_tree_node(*tree, value);
-		return (*tree);
-	}
-
-	return (avl_insert2(tree, value));
 }
